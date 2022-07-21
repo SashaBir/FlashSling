68a6e7f457b11503030d80193675e7701bae60fb
Sasha <sashakrin.bir@mail.ru>
2022-07-22 01:53:32 +0300

    add: Ui

---
+++ Assets/Project/Scripts/Animations/MovingRepeater.cs
@@ -0,0 +1,29 @@
+﻿using Cysharp.Threading.Tasks;
+using DG.Tweening;
+using UnityEngine;
+
+public class MovingRepeater : MonoBehaviour
+{
+    [SerializeField] private Vector2 _forward;
+    [SerializeField] [Min(0)] private float _fromDuration;
+    [SerializeField] [Min(0)] private float _toDuration;
+    
+    private Tween _tween;
+    
+    private void Awake() => Repeat();
+
+    private async UniTaskVoid Repeat()
+    {
+        while (true)
+        {
+            var initial = transform.position;
+            
+            Vector3 target = initial + (Vector3)_forward;
+            _tween = transform.DOMove(target, _toDuration).SetEase(Ease.Linear);
+            await UniTask.WaitWhile(() => _tween.IsPlaying() == true);
+            
+            _tween = transform.DOMove(initial, _fromDuration).SetEase(Ease.Linear);
+            await UniTask.WaitWhile(() => _tween.IsPlaying() == true);
+        }
+    }
+}
---
+++ Assets/Project/Scripts/Di/PlayerInstaller.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public class PlayerInstaller : MonoBehaviour
+{
+
+}
